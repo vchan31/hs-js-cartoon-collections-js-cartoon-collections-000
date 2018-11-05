89a24364f8073c85a13e2d24c8036cb4a2069917
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 19:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function dwarfRollCall(dwarves) {
 var numberedDwarves = [];
 
-for (let i = 0; i < dwarves.length; i++ ) 
+for (let i = dwarves.length/2; i < dwarves.length; i++ ) 
+
 {numberedDwarves.push((i+1) + '. ' + dwarves[i]);
+
 } 
 
 return numberedDwarves.join(" ");
@@ -31,7 +33,18 @@ function findTheCheese (foods) {
      if (foods[i] === 'cheddar') {return 'cheddar'}
      else if (foods[i] === 'gouda') {return 'gouda'}
      else if (foods[i] === 'camembert') {return 'camembert'}
+     else if (foods[i] === 'swiss') {return 'swiss'}
     
    }
   return 'no cheese!'
 }
+
+
+function wordsWithB (array1) {
+  var list = []
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i][0] === 'b') { list.push(array[i])}
+    
+  }return list
+}
+
